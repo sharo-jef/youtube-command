07c4cf70608b475ea45b25e314ee54cb9c587d47
sharo-jef <cleared.for.approach.runway23@gmail.com>
2021-04-29 03:00:49 +0900

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node -r dotenv/config
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import { Youtube } from './youtube.js';
+
+const youtube = new Youtube(process.env.API_KEY);
+
+yargs(hideBin(process.argv))
+    .scriptName('youtube')
+    .locale('en')
+    .demandCommand()
+    .command('channel <id>', 'get channel data', argv => {
+        return argv
+            .positional('id', {
+                type: 'string',
+                desc: 'channel id',
+            })
+            .option('full', {
+                alias: ['f'],
+                desc: 'return full data',
+                type: 'boolean',
+            });
+    }, async argv => console.log(JSON.stringify(await youtube.channel(argv))))
+    .argv;
---
+++ youtube.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import log4js from 'log4js';
+
+const logger = log4js.getLogger('youtube');
+logger.level = 'all';
+
+export class Youtube {
+    /** @type {string} */
+    key = '';
+    /**
+     * @param {string} key api key
+     */
+    constructor(key) {
+        this.key = key;
+    }
+    async channel(argv) {
+        const config = {
+            params: {
+                channelId: argv.id,
+                part: argv.full ? 'snippet' : 'id',
+                maxResults: 50,
+            },
+        };
+        const result = await this._getData('https://www.googleapis.com/youtube/v3/search', config) || [];
+        return result
+            .filter(d => d?.id?.videoId)
+            .map(d => {
+                const tmp = { id: d?.id?.videoId };
+                if (d?.snippet) {
+                    tmp.title = d?.snippet?.title;
+                }
+                return tmp;
+            });
+    }
+    async _getData(url, config = {}) {
+        let result = [];
+
+        if (config) {
+            config.params.key = this.key;
+            config.params.quotaUser = `${new Date().getTime()}`;
+        }
+
+        const response = await axios.get(url, config)
+            .catch(error => logger.error(error?.response?.data));
+
+        if (response?.data) {
+            if (response.data?.items) {
+                result = [...result, ...response.data.items];
+            }
+            if (response.data?.nextPageToken) {
+                config.params.pageToken = response.data.nextPageToken;
+                result = [...result, ...await this._getData(url, config)];
+            }
+        }
+
+        return result;
+    }
+}
